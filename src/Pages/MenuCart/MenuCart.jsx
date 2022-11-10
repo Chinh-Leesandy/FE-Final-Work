2f1c95c2c2c8990845fb2ebd614667ed2eb6321e
NguyenHongAnh0812 <nguyenhonganhron@gmail.com>
2022-11-10 21:52:25 +0700

    Update MenuCart First

--- src/Pages/MenuCart/MenuCart.jsx
+++ src/Pages/MenuCart/MenuCart.jsx
@@ -6,6 +6,12 @@ import { Link } from "react-router-dom";
 
 export default function MenuCart({styleMenuCart,setStyleMenuCart}) {
     const listItems = useSelector(state => state.product.listItems);
+    function CloseMenuCart() {
+      setStyleMenuCart(preStyleMenuCart => {
+          const styleMenuCart = {...preStyleMenuCart, right: "-400px"}
+          return styleMenuCart
+      })
+  }
     // function RemoveItem(e) {
     //     let item = getItem(e.target)
     //     setListItems(preListItems => {
@@ -45,81 +51,79 @@ export default function MenuCart({styleMenuCart,setStyleMenuCart}) {
     //         return listItems
     //     })
     // }
-    return (
-    <React.Fragment>
-        {/* <div className="background-wrap-menu-cart" style={{width: window.innerWidth, height: window.innerWidth,
-        display: `${(styleMenuCart.right == "-400px")?("none"):("block")}`}} >
-        </div> */}
-        <div className="menu-cart-wrap" >
-            <div className="menu-cart">
-                <div className="menu-cart__header">
-                    <div className="menu-cart__header-title">
-                        Cart: 1 Items 
+    console.log(listItems)
+      return (
+        <React.Fragment>
+            <div className="background-wrap-menu-cart" style={{width: window.innerWidth, height: window.innerWidth,
+            display: `${(styleMenuCart.right == "-400px")?("none"):("block")}`}} onClick = {CloseMenuCart}>
+            </div>
+            <div className="menu-cart-wrap" style={{right: styleMenuCart.right, height: "100%"}}>
+                <div className="menu-cart">
+                    <div className="menu-cart__header">
+                        <div className="menu-cart__header-title">
+                            Cart:  Items 
+                        </div>
+                        <div className="menu-cart__header-close" onClick={CloseMenuCart}>
+                        <i className ="fa-solid fa-xmark btnclose"></i>
+                        </div>
                     </div>
-                    <div className="menu-cart__header-close" >
-                        <Link to="/Shop">
-                    <i className ="fa-solid fa-xmark btnclose"></i>
-
-                        </Link>
-                  </div>
-                <div className = "menu-cart__body">
-                {
-                    (
-                    listItems.length > 0 && 
-                    listItems.map(([key,value],idx) => {
-                        return (
-                            <div className="menu-cart-item" key={idx} id = {key.id}>
-                                <div className="product-quantity">
-                                    <button className ="quantity-btn quantity-btn--active">
-                                        <i className ="fa-solid fa-plus"></i>
-                                    </button>
-                                    <span>{value}</span>
-                                    {
-                                        (    
-                                            value == 1 && 
-                                            <button className = "quantity-btn">
-                                                <i className ="fa-solid fa-minus"></i>
-                                            </button>
-                                        )
-                                        ||
-                                        (
-                                            <button className ="quantity-btn quantity-btn--active" >
-                                                <i className ="fa-solid fa-minus"></i>
-                                            </button>
-                                        )
-                                    }
-                                </div>
-                                <div className="product-info">
-                                    <div className="product-img" style={{backgroundImage: `url(${key.images})`}}></div>
-                                    <div className="product-description">
-                                        <div className = "product-name"> {key.product_name}</div>
-                                        <div className = "product-price"> {key.price.toLocaleString('vi')}đ x {value}</div>
-                                        <div className = "product-total-price">{(value*key.price).toLocaleString('vi')}đ</div>
+                    <div className = "menu-cart__body">
+                    {
+                        (
+                        listItems.length > 0 && 
+                        listItems.map((items) => {
+                            return (
+                                <div className="menu-cart-item">
+                                    <div className="product-quantity">
+                                        <button className ="quantity-btn quantity-btn--active" >
+                                            <i className ="fa-solid fa-plus"></i>
+                                        </button>
+                                        <span>{0}</span>
+                                        {
+                                            // (    
+                                            //     value == 1 && 
+                                            //     <button className = "quantity-btn">
+                                            //         <i className ="fa-solid fa-minus"></i>
+                                            //     </button>
+                                            // )
+                                            // ||
+                                            (
+                                                <button className ="quantity-btn quantity-btn--active" >
+                                                    <i className ="fa-solid fa-minus"></i>
+                                                </button>
+                                            )
+                                        }
+                                    </div>
+                                    <div className="product-info">
+                                        <div className="product-img" style={{backgroundImage: `url(https://petsla-api.herokuapp.com${items.images})`}}></div>
+                                        <div className="product-description">
+                                            <div className = "product-name"> {items.product_name}</div>
+                                            <div className = "product-price"> {items.price.toLocaleString('vi')}đ x {0}</div>
+                                            <div className = "product-total-price">{(items.price).toLocaleString('vi')}đ</div>
+                                        </div>
+                                    </div>
+                                    <div className="product-button-close" >
+                                        <i className ="fa-solid fa-xmark btnclose"></i>
                                     </div>
                                 </div>
-                                <div className="product-button-close" >
-                                    <i className ="fa-solid fa-xmark btnclose"></i>
-                                </div>
+                            )
+                        }) 
+                        )
+                        || 
+                        (
+                            <div className="no-products">
+                                <img className="no-products-img" src = "https://www.leoasher.dev/static/media/sadCat.2335333f.png" />
+                                <h3 className="no-products-title"> There's no item in cart! </h3>
                             </div>
                         )
-                    }) 
-                    )
-                    
-                    (
-                        <div className="no-products">
-                            <img className="no-products-img" src = "https://www.leoasher.dev/static/media/sadCat.2335333f.png" />
-                            <h3 className="no-products-title"> There's no item in cart! </h3>
-                        </div>
-                    )
-                }
-                </div>
-                <div className="menu-cart__footer">
-                    <button type = "button" className="menu-cart-btn btn-primary">Checkout </button>
-                    <button type = "button" className="menu-cart-btn btn-view-cart">View Cart</button>
-                </div>
-            </div> 
-        </div>
-      </div>
-    </React.Fragment>
-  );
+                    }
+                    </div>
+                    <div className="menu-cart__footer">
+                        <button type = "button" className="menu-cart-btn btn-primary">Checkout ()</button>
+                        <button type = "button" className="menu-cart-btn btn-view-cart">View Cart</button>
+                    </div>
+                </div> 
+            </div>
+        </React.Fragment>
+        )
 }
