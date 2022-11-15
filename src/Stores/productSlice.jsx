90051ed857b95cb7862231a250262b24c818da64
Chinh-Leesandy <maichinh17092002@gmail.com>
2022-11-15 11:06:20 +0700

    Chinh conflitc 2

--- src/Stores/productSlice.jsx
+++ src/Stores/productSlice.jsx
@@ -7,48 +7,51 @@ const productSlice = createSlice({
     listItems: [],
     count: 0,
     totalPrice: 0,
-    productItem : {},
+    idProductItem : {},
+    Products : [], //luu tru du lieu product goc
+    Sort : "0"
   },
   reducers: {
-    callApi(state, data) {
-      state.product = data.payload;
+    callApi(state, actions) {
+      state.product = actions.payload;
+      state.Products = actions.payload;
     },
     setSort(state, actions) {
       state.Sort = actions.payload;
     },
-    addProduct(state, data) {
+    addProduct(state, actions) {
       let dem = 0;
       state.listItems.forEach(element => {
-        if (element.value.id === data.payload.id) {
+        if (element.value.id === actions.payload.id) {
           element.count += 1;
           state.count += 1;
-          state.totalPrice += data.payload.price;
+          state.totalPrice += actions.payload.price;
           dem = 1;
         }
       });
       if (dem === 0) {
         state.listItems.push(
           {
-            value: data.payload,
+            value: actions.payload,
             count: 1
           }
         );
         state.count += 1;
-        state.totalPrice += data.payload.price;
+        state.totalPrice += actions.payload.price;
       }
     },
-    removeItem(state, id) {
+    removeItem(state, actions) {
       const idx = state.listItems.findIndex(element => {
-        return element.value.id == id.payload
+        return element.value.id == actions.payload
 
       })
       state.count -= state.listItems[idx].count;
       state.totalPrice = state.totalPrice - state.listItems[idx].count * state.listItems[idx].value.price;
       state.listItems.splice(idx, 1);
     },
-    increaseItem(state, id) {
+    increaseItem(state, actions) {
       state.listItems.forEach(element => {
-        if (element.value.id == id.payload) {
+        if (element.value.id == actions.payload) {
           element.count += 1;
           state.count += 1;
           state.totalPrice += element.value.price;
@@ -56,9 +59,9 @@ const productSlice = createSlice({
       })
       
     },
-    decreaseItem(state, id) {
+    decreaseItem(state, actions) {
       state.listItems.forEach(element => {
-        if (element.value.id == id.payload) {
+        if (element.value.id == actions.payload) {
           element.count -= 1;
           state.count -= 1;
           state.totalPrice += element.value.price;
@@ -66,10 +69,10 @@ const productSlice = createSlice({
 
       })
     },
-    productItems(state, data){
-        state.productItem = data.payload;
-    }
+    addIdProductItems(state, actions){
+      state.idProductItem = actions.payload;
+    },
   },
 });
-export const { callApi, setSort, addProduct, removeItem, increaseItem, decreaseItem, productItems } = productSlice.actions;
+export const { callApi, setSort, addProduct, removeItem, increaseItem, decreaseItem, addIdProductItems} = productSlice.actions;
 export default productSlice.reducer;
