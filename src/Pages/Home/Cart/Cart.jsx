238eccf57404dc91586248e3b817ab02415b6cbb
NguyenHongAnh0812 <nguyenhonganhron@gmail.com>
2022-11-16 16:46:16 +0700

    Update

--- src/Pages/Home/Cart/Cart.jsx
+++ src/Pages/Home/Cart/Cart.jsx
@@ -12,38 +12,61 @@ export default function Cart() {
             <Header></Header>
             <div className="cart_main">
                 <div className="cart_items">
-                        {
-                            (
-                                listItems.length > 0 &&
-                                listItems.map((items) => {
-                                    return (
-                                            <div className="cart-item">
-                                                <div className="cart_img">
-                                                    <img
-                                                        className="d-block w-100"
-                                                        src={`https://petsla-api.herokuapp.com${items.value.images}`}
-                                                        alt="Image One"
-                                                    />
-                                                </div>
-                                                <div className="cart_title">
-                                                    <h3>{items.value.product_name}</h3>
-                                                    <p>{items.value.description}</p>
-                                                </div>
-                                            </div>
-                                    )
-                                })
-                            )
-                            ||
-                            (
-                                <div className="no-products">
-                                    <img className="no-products-img" src="https://www.leoasher.dev/static/media/sadCat.2335333f.png" />
-                                    <h3 className="no-products-title"> There's no item in cart! </h3>
-                                </div>
-                            )
-                        }
+                    {
+                        (
+                            listItems.length > 0 &&
+                            listItems.map((items) => {
+                                return (
+                                    <div className="cart-item">
+                                        <div className="cart-quantity">
+                                            <button className="quantity-btn quantity-btn--active"  >
+                                                <i className="fa-solid fa-plus"></i>
+                                            </button>
+                                            <span >{items.count}</span>
+                                            {
+                                                (
+                                                    items.count === 1 &&
+                                                    <button className="quantity-btn">
+                                                        <i className="fa-solid fa-minus"></i>
+                                                    </button>
+                                                )
+                                                ||
+                                                (
+                                                    <button className="quantity-btn quantity-btn--active" >
+                                                        <i className="fa-solid fa-minus"></i>
+                                                    </button>
+                                                )
+                                            }
+                                        </div>
+                                        <div className="cart_img">
+                                            <img
+                                                className="d-block w-100"
+                                                src={`https://petsla-api.herokuapp.com${items.value.images}`}
+                                                alt="Image One"
+                                            />
+                                            <div className="cart-total-price">{(items.count * items.value.price).toLocaleString('vi')}đ</div>
+                                        </div>
+                                        <div className="cart_title">
+                                            <h3>{items.value.product_name}</h3>
+                                            <p>{items.value.description}</p>
+                                        </div>
+                                        <div className="cart_close" >
+                                            <i className="fa-solid fa-xmark btnclose"></i>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        )
+                        ||
+                        (
+                            <div className="no-cart">
+                                <img className="no-cart-img" src="https://www.leoasher.dev/static/media/sadCat.2335333f.png" />
+                                <h3 className="no-cart-title"> There's no item in cart! </h3>
+                            </div>
+                        )
+                    }
                 </div>
                 <div className="cart_check">
-
                 </div>
             </div>
             <Footer></Footer>
