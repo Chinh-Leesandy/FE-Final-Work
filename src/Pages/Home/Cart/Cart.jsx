7ebf5b16c5756d10f0c1fdcdc5aab163548752ad
Nguyễn Hồng Ánh <98610210+NguyenHongAnh0812@users.noreply.github.com>
2022-11-17 12:15:00 +0700

    Anh (#37)
    * test
    
    * Update
    
    * Update CSS
    
    * update
    
    * update
    
    * Update call api Menucart by Anh
    
    * Update Grid.css by Anh
    
    * update later
    
    * Update MenuCart First
    
    * Update Done MenuCart By Anh
    
    * fix
    
    * update
    
    * update
    
    * update
    
    * Update
    
    * update
    
    * update
    
    * update
    
    Co-authored-by: NguyenHongAnh0812 <nguyenhonganh@gmail.com>
    Co-authored-by: Chinh-Leesandy <maichinh17092002@gmail.com>
    Co-authored-by: Chinh-Leesandy <101963688+Chinh-Leesandy@users.noreply.github.com>

--- src/Pages/Home/Cart/Cart.jsx
+++ src/Pages/Home/Cart/Cart.jsx
@@ -1,82 +1,94 @@
 import React, { useState, useEffect, useRef } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import Carousel from 'react-bootstrap/Carousel';
 import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
 import "./Cart.css"
 import { useDispatch, useSelector } from "react-redux";
+import { increaseItem, decreaseItem, removeItem } from "../../../Stores/productSlice";
 // import { set } from 'immer/dist/internal';
+function getItem(target) {
+    let item = target
+    while (!item.classList.contains("menu-cart-item")) {
+        item = item.parentElement
+    }
+    return item
+}
 export default function Cart() {
     const listItems = useSelector(state => state.product.listItems);
-    const [counts, setCounts] = useState(0)
-    const handleClickNext = () => {
-        if (counts == listItems.length - 1) setCounts(0);
-        else setCounts(counts => counts + 1)
+    const dispatch = useDispatch();
+    function RemoveItem(e) {
+        let item = getItem(e.target)
+        dispatch(removeItem(item.getAttribute("id")));
+        console.log(item.getAttribute("id"))
     }
-    const handleClickPrevious = () => {
-        if (counts == 0) setCounts(counts => listItems.length-1);
-        else setCounts(counts => counts - 1)
-    }
+    function IncreaseItem(e) {
+        let item = getItem(e.target)
+        dispatch(increaseItem(item.getAttribute("id")));
+        console.log(item.getAttribute("id"))
+    };
+    function DecreaseItem(e) {
+        let item = getItem(e.target)
+        dispatch(decreaseItem(item.getAttribute("id")));
+        console.log(item.getAttribute("id"))
+    };
     return (
         <React.Fragment>
             <Header></Header>
-            <div className="cart_main">
-                <div className="cart_items" >
-                    {
-                        (
-                            listItems.length !== 0 &&
-                            <div className="cart-item" >
-                                    <a href="#" class="previous round" onClick={handleClickPrevious}>&#8249;</a>
-                                    <a href="#" class="next round" onClick={handleClickNext}>&#8250;</a>
-                                <div className="cart_img">
-                                    <img
-                                        className="d-block w-100"
-                                        src={`https://petsla-api.herokuapp.com${listItems[counts].value.images}`}
-                                        alt="Image One"
-                                    />
-                                    {/* <div className="cart-total-price">{(items.count * items.value.price).toLocaleString('vi')}đ</div> */}
-                                </div>
-                                <div className="cart_title">
-                                    <h5>{listItems[counts].value.product_name}</h5>
-                                    <p>{listItems[counts].value.description}</p>
-                                    <div className="cart-price"> {listItems[counts].value.price.toLocaleString('vi')}đ x {listItems[counts].count}</div>
-                                    <div className="cart-total-price">{(listItems[counts].count * listItems[counts].value.price).toLocaleString('vi')}đ</div>
-                                    <div className="cart-quantity">
-                                        <button className="quantity-btn quantity-btn--active"  >
-                                            <i className="fa-solid fa-plus"></i>
-                                        </button>
-                                        <span className="cart-quantity_cout"  >{listItems[counts].count}</span>
-                                        {
-                                            (
-                                                listItems[counts].count === 1 &&
-                                                <button className="quantity-btn">
-                                                    <i className="fa-solid fa-minus"></i>
+            <div className="cart__container">
+                <div className="cart_main">
+                    <div className="cart_main-items">
+                        <div className="cart_main-items--header">Cart</div>
+                        {
+                            (
+                                listItems.length != 0 &&
+                                listItems.map((items) => {
+                                    return (
+                                        <div className="cart-item" key={items.value.id} id={items.value.id}>
+                                            <div className="cart-quantity">
+                                                <button className="quantity-btn quantity-btn--active" onClick={IncreaseItem} >
+                                                    <i className="fa-solid fa-plus"></i>
                                                 </button>
-                                            )
-                                            ||
-                                            (
-                                                <button className="quantity-btn quantity-btn--active" >
-                                                    <i className="fa-solid fa-minus"></i>
-                                                </button>
-                                            )
-                                        }
-                                    </div>
+                                                <span>{items.count}</span>
+                                                {
+                                                    (
+                                                        items.count === 1 &&
+                                                        <button className="quantity-btn">
+                                                            <i className="fa-solid fa-minus"></i>
+                                                        </button>
+                                                    )
+                                                    ||
+                                                    (
+                                                        <button className="quantity-btn quantity-btn--active" onClick={DecreaseItem}>
+                                                            <i className="fa-solid fa-minus"></i>
+                                                        </button>
+                                                    )
+                                                }
+                                            </div>
+                                            <div className="cart-info">
+                                                <div className="cart-img" style={{ backgroundImage: `url(https://petsla-api.herokuapp.com${items.value.images})` }}></div>
+                                                <div className="cart-description">
+                                                    <div className="cart-name"> {items.value.product_name}</div>
+                                                    <div className="cart-price"> {items.value.price.toLocaleString('vi')}đ x {items.count}</div>
+                                                    <div className="cart-total-price">{(items.count * items.value.price).toLocaleString('vi')}đ</div>
+                                                </div>
+                                            </div>
+                                            <div className="cart-button-close" onClick={RemoveItem} >
+                                                <i className="fa-solid fa-xmark btnclose"></i>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                                )
+                            )
+                            ||
+                            (
+                                <div className="no-products">
+                                    <img className="no-products-img" src="https://www.leoasher.dev/static/media/sadCat.2335333f.png" />
+                                    <h3 className="no-products-title"> There's no item in cart! </h3>
                                 </div>
-                                <div className="cart_close" >
-                                    <i className="fa-solid fa-xmark btnclose"></i>
-                                </div>
-                            </div>
-                        )
-                        ||
-                        (
-                            <div className="no-cart">
-                                <img className="no-cart-img" src="https://www.leoasher.dev/static/media/sadCat.2335333f.png" />
-                                <h3 className="no-cart-title"> There's no item in cart! </h3>
-                            </div>
-                        )
-                    }
-                </div>
-                <div className="cart_check">
+                            )
+                        }
+                    </div>
+                    <div className="cart_main-checkout"></div>
                 </div>
             </div>
             <Footer></Footer>
