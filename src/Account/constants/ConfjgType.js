1c4429327298f6fb1002106daa390f06977522eb
DUNG1604 <dung0373691479@gmail.com>
2022-11-18 14:47:21 +0700

    update login by dung

--- src/Account/constants/ConfjgType.js
+++
@@ -1,8 +0,0 @@
-export const SHOW_ITEM = 'SHOW_ITEM'
-export const LOGIN= "LOGIN"
-export const STATUS = "STATUS"
-export const PRODUCT_DETAIL ="PRODUCT_DETAIL"
-export const SIGNUP = "SIGNUP"
-export const SHOW_PROFILE = 'SHOW_PROFILE'
-export const GET_TOKEN = "GET_TOKEN"
-export const DELETE_TOKEN = "DELETE_TOKEN"
